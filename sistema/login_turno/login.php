6d4d6bbd4277fb0625aa2a682ab99c5882dfa2e5
maikel schroetlin <maikelhenery@gmail.com>
2015-04-16 19:02:32 -0400

    #3 login del sistema de turnos.

---
+++ sistema/login_turno/login.php
@@ -0,0 +1,22 @@
+﻿<?php ?>
+<meta charset="UTF-8">
+<center>
+<form action="validar_usuario.php" method="post">
+	<table>
+		<tr>
+			<td>Usuario:</td>
+			<td><input type="text" name="admin" required="required"></td>
+		</tr>
+		<tr>
+			<td>Password:</td>
+			<td><input type="password" name="password_usuario" required="required"></td>
+		</tr>
+		<tr>
+			<td colspan="2"><center><input type="submit" value="Iniciar Sesión" name="iniciar"></center></td>
+			
+		</tr>
+	</table>
+</form>
+</center>
+<?
+?>
---
+++ sistema/login_turno/validar_usuario.php
@@ -0,0 +1,56 @@
+﻿<?php
+
+mysql_connect('localhost','root','')or die ('Ha fallado la conexión: '.mysql_error());
+
+mysql_select_db('reserva_turno')or die ('Error al seleccionar la Base de Datos: '.mysql_error());
+  
+
+$usuario = $_POST["admin"];   
+$password = $_POST["password_usuario"];
+ 
+
+$result = mysql_query("SELECT * FROM administradores WHERE nombre_administrador = '$usuario'");
+ 
+
+if($row = mysql_fetch_array($result))
+{     
+
+ if($row["password"] == $password)
+ {
+
+  session_start();  
+
+  $_SESSION['usuario'] = $usuario;  
+
+  header("Location: index.php");  
+ }
+ else
+ {
+ 
+  ?>
+   <script languaje="javascript">
+    alert("Contraseña Incorrecta");
+    location.href = "login.php";
+   </script>
+  <?php
+             
+ }
+}
+else
+{
+
+?>
+ <script languaje="javascript">
+  alert("El nombre de usuario es incorrecto!");
+  location.href = "login.php";
+ </script>
+<?php 
+         
+}
+ 
+
+mysql_free_result($result);
+ 
+
+mysql_close();
+?>
